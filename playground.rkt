eb40935a4f323ec6958745be5cb6c141b1346cab
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2023-02-14 15:06:04 +0200

    Memoization

--- playground.rkt
+++ playground.rkt
@@ -1,35 +1,22 @@
 #lang racket
-(provide (all-defined-out))
-(define x 3)
-(define y (+ x 2))
 
-(define cube1
-  (lambda (x)
-    (* x x x)))
+(define pow-of-two
+  (letrec ([f (lambda (x) (cons x (lambda () (f (* x 2)))))])
+    (lambda () (f 2))))
 
-(define (pow x y)
-  (if (= y 0)
-      1
-      (* x (pow x (- y 1)))))
 
-(define test-list (list 1 2 3 4))
-(define test-list2 (list 5 6 7 8))
+(define fibonacci3
+  (letrec([memo null]  
+          [f (lambda (x)
+               (let ([ans (assoc x memo)])
+                 (if ans
+                     (cdr ans)
+                     (let ([new-ans (if (or (= x 1) (= x 2))
+                                        1
+                                        (+ (f (- x 1))
+                                           (f (- x 2))))])
+                       (begin
+                         (set! memo (cons (cons x new-ans) memo))
+                         new-ans)))))])
+    f))
 
-(define (sum xs)
-  (if (null? xs)
-      0
-      (+ (car xs) (sum (cdr xs)))))
-
-(define (my-append xs ys)
-  (if (null? xs)
-      ys
-      (cons (car xs) (my-append (cdr xs) ys))))
-
-(define (my-map f xs)
-  (if (null? xs)
-      null
-      (cons (f (car xs))
-            (my-map f (cdr xs)))))
-
-(define (double x)
-  (* x 2))
